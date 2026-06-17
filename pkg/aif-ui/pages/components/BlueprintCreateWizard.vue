bef6513e0fbad37deb097f8786f1c95b9c75cef0
Aline Werner <aline.werner@suse.com>
2026-06-17 17:11:11 -0300

    feat: persist source on UI-created blueprints, add display helper

--- pkg/aif-ui/pages/components/BlueprintCreateWizard.vue
+++ pkg/aif-ui/pages/components/BlueprintCreateWizard.vue
@@ -77,6 +77,7 @@ async function onCreate() {
       displayName: basicInfo.value.displayName,
       version:     basicInfo.value.version,
       description: basicInfo.value.description || undefined,
+      source:      props.prefill?.source ?? 'Custom',
       components:  components.value,
     };
     await createBlueprint(spec);
@@ -92,6 +93,7 @@ const reviewForm = computed<BlueprintSpec>(() => ({
   displayName: basicInfo.value.displayName,
   version:     basicInfo.value.version,
   description: basicInfo.value.description || undefined,
+  source:      props.prefill?.source ?? 'Custom',
   components:  components.value,
 }));
 </script>
